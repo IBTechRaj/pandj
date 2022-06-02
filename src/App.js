fa219fac261f82c74f82416dcce1192085b301d6
ibtechraj <krs30018@gmail.com>
2022-06-02 20:58:01 +0530

    home page done

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import "./App.css";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Home,
+  About,
+  Contact,
+  Blog,
+  Posts,
+  Post,
+} from "./components";
+
+export default function App() {
+return (
+<div >
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>
+  </div>
+)
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer className="py-5 bg-dark fixed-bottom">
+      <footer className="py-2 bg-dark fixed-bottom">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2020
+            Copyright &copy; Peace And Joy Fellowship Church 2022
           </p>
         </div>
       </footer>
